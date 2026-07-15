79e4dfaa37446577c5401b9c27d021339d960dc7
Lakshay Raj <lakshaywork012@gmail.com>
2026-07-15 22:37:36 +0530

    Athmetic operator in Cpp

---
+++ arthmetic_operator.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Arthmetic Operators in Cpp" << endl;
+    
+
+
+    return 0;
+}
--- variables.cpp
+++ variables.cpp
@@ -9,6 +9,11 @@ int main() {
     float y = 5.99; // Floating point variable
     char z = 'D'; // Character variable
 
+    // Mutiple variables can be declared in a single line
+    float a , b ;
+    a = 10; 
+    b = 20; 
+
     // cout << x;
     // cout << "\n";
 
@@ -16,6 +21,26 @@ int main() {
     cout << "Float y = " << y << endl;
     cout << "Character z = " << z << endl;
 
+    cout<< "Multiplication of x and y is: " << x << " * " << y << " = " << x * y << endl;
+
+    // We can also use C style printf function to print the output in Cpp
+    printf("Multiplication of x and y is: %d * %2.f = %2.f\n", x, y, x * y);
+
+    printf("division of a and b is:");
+    cout << a / b << endl;
+
+
+    // Updation of Variables in Cpp
+
+    cout << "Before updating the value of x: " << x << endl;
+
+    x += 5; // x = x + 5
+    cout << "After updating the value of x: " << x << endl;
+    cout << "Again updating the value of x: " << x << endl;
+
+    x = 15 ;
+
+    cout << "After again updating the value of x: " << x << endl;    
 
     return 0;
 
