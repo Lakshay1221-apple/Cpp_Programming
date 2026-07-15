6d9b17c6e710d9af040cd84bb53403269a31bb9d
Lakshay Raj <lakshaywork012@gmail.com>
2026-07-15 22:18:25 +0530

    Variables in Cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 using namespace std;
-int main() {
+int main () {
+    // Cout is used for starting the line and endl is used for ending the line 
 
-    cout << "Hello, World!" << endl;
+    cout<< "Hello world " << endl;
+    cout << "the product of 5 and 10 is: " << 5 * 10 << endl;
     return 0;
-    
 }
---
+++ variables.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Variables are used to store data, so that we can use it later in the programing
+    cout << "Variables in Cpp" << endl;
+
+    int x = 5; // Integer variable
+    float y = 5.99; // Floating point variable
+    char z = 'D'; // Character variable
+
+    // cout << x;
+    // cout << "\n";
+
+    cout << "Integer x = " << x << endl;
+    cout << "Float y = " << y << endl;
+    cout << "Character z = " << z << endl;
+
+
+    return 0;
+
+}
