7ff4855768aa56f39979a4eefd49d2e58db232d2
Lakshay Raj <lakshaywork012@gmail.com>
2026-07-16 09:57:52 +0530

    Operators in Cpp

--- arthmetic_operator.cpp
+++ arthmetic_operator.cpp
@@ -2,6 +2,16 @@
 using namespace std;
 int main() {
     cout << "Arthmetic Operators in Cpp" << endl;
+
+    float a , b;
+    a = 10; 
+    b = 8;
+
+    cout<< "Addition of a and b is: " << a << " + " << b << " = " << a + b << endl;
+    cout<< "Subtraction of a and b is: " << a << " - " << b << " = " << a - b << endl;
+    cout<< "Multiplication of a and b is: " << a << " * " << b << " = " << a * b << endl;
+    cout<< "Division of a and b is: " << a << " / " << b << " = " << a / b << endl;
+    cout<< "Modulus of a and b is: " << a << " % " << b << " = " << (int)a % (int)b << endl;
     
 
 
