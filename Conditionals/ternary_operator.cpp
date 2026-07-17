0ff862e79725f736268d17313f1e700da7861aa2
Lakshay Raj <lakshaywork012@gmail.com>
2026-07-17 22:34:23 +0530

    Practise Ternary Operator in Cpp

--- Conditionals/ternary_operator.cpp
+++ Conditionals/ternary_operator.cpp
@@ -2,7 +2,10 @@
 using namespace std;
 int main () {
 
+    int a;
+    printf("Enter a number: ");
+    cin >> a; 
+    cout << ((a % 2 ==0) ? "the number is even" : "the number is odd") << endl;
 
-    
     return 0;
 }
