85daa5f2326a376d30dc586828caa61ca953add6
Lakshay Raj <lakshaywork012@gmail.com>
2026-07-18 10:10:23 +0530

    Prime and composite number loop

--- loops/for_loop.cpp
+++ loops/for_loop.cpp
@@ -6,6 +6,28 @@ int main() {
         cout << "Iteration: " << i << endl;
     }
 
-    return 0;
+    int num ;
+    cout << "Enter a number: ";
+    cin >> num;
+    bool isPrime = true;
 
+    if (num < 0){
+        cout<< "the number is neither prime nor composite"<<endl;
+    }
+    
+    for (int i = 2; i <= num / 2; i++){
+        if (num % i == 0){
+            isPrime = false;
+            break;
+        }
+    }
+
+    if(isPrime){
+        cout << num << " is a prime number." << endl;
+    } else {
+        cout << num << " is not a prime number." << endl;
+    }  
+
+    return 0;
 }
+
