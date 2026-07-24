7ea21c752a71a297639cdb8096d2d7daacf4992c
Lakshay Raj <lakshaywork012@gmail.com>
2026-07-24 23:31:12 +0530

    Array programming questions in C++

--- pattern_printing/sq_star.cpp
+++ pattern_printing/sq_star.cpp
@@ -2,10 +2,9 @@
 using namespace std;
 int main () {
 
-    for (int i = 1 ;  i <= 5 ; i++){ // this loop is for rows
-        
+    for (int i = 1 ;  i <= 5 ; i++){ // this loop is for rows        
         for (int j = 1 ; j <= 3 ; j++){ // this loop is for columns
-            cout << "*";
+            cout << j;
         }
         cout << endl;
         
