acbe1b4975006c620633fa32627bd5f02c10b250
Lakshay Raj <lakshaywork012@gmail.com>
2026-09-21 21:51:54 +0530

    default argument in cpp

---
+++ default_argument.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int add(int a , int b = 10);
+
+int add(int a , int b){
+    return a  + b;
+}
+
+int main () {
+    cout << add(2 , 10) << endl;
+    cout << add(20) << endl;
+
+    return 0;
+}
